70906eb7cf7f0a02656b79cd188f62a2ab539271
Sounak Gupta <sounak.besu@gmail.com>
2020-08-17 02:54:02 -0700

    added cmdline args

--- parse.py
+++ parse.py
@@ -7,6 +7,7 @@ from bibtexparser.bparser import BibTexParser
 from bibtexparser.customization import *
 #from bibtexparser.customization import homogenize_latex_encoding
 from bibtexparser.customization import convert_to_unicode
+import sys, getopt
 
 def customizations(record):
     """Use some functions delivered by the library
@@ -23,12 +24,29 @@ def customizations(record):
     record = doi(record)
     return record
 
-files = glob.glob("papers" + '/**/*.bib', recursive=True)
-for bibfile in files:
-    with open(bibfile, encoding='utf-8') as bibtex_file:
-        parser = BibTexParser()
-        parser.customization = customizations
-        #parser.customization = homogenize_latex_encoding
-        parser.customization = convert_to_unicode
-        bib_database = bibtexparser.load(bibtex_file, parser=parser)
-        print(bib_database.entries)
+def main(argv):
+    rootpath = ''
+    try:
+        opts, args = getopt.getopt(argv,"hp:",["path="])
+    except getopt.GetoptError:
+        print("parse.py -p <rootpath>")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print("parse.py -p <rootpath>")
+            sys.exit()
+        elif opt in ("-p", "--path"):
+            rootpath = arg
+
+    files = glob.glob(rootpath + '/**/*.bib', recursive=True)
+    for bibfile in files:
+        with open(bibfile, encoding='utf-8') as bibtex_file:
+            parser = BibTexParser()
+            parser.customization = customizations
+            #parser.customization = homogenize_latex_encoding
+            parser.customization = convert_to_unicode
+            bib_database = bibtexparser.load(bibtex_file, parser=parser)
+            print(bib_database.entries)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
